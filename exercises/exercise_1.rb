4cc4999b2d758f494a1c0965215c3fc36743cb7a
ekeneezeani <108833282+ekeneezeani@users.noreply.github.com>
2022-10-18 00:07:19 -0500

    completed 4 exercises

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,20 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+store = Store.new
+store.name = "Burnaby"
+store.annual_revenue = 300000
+store.mens_apparel = true
+store.womens_apparel = true
+store.save
+
+store = Store.create(name:"Richmond", annual_revenue:1260000,
+mens_apparel: false, womens_apparel: true)
+
+store = Store.create(name:"Gastown", 
+annual_revenue:190000, mens_apparel: true,
+womens_apparel: false)
+
+store = Store.all
+puts store.count
+
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+
+@store1.name = "Duffrin"
+@store1.save
+# store = Store.find_by(name:"Gastown")
+# store.name = "Packdale"
+# store.save
